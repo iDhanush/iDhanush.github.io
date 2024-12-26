260f2d3473f1ed34cd9eb7e4e23de20c970b2ad0
iDhanush <idhanushm@gmail.com>
2024-12-26 19:26:37 +0530

    icon change

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function () {
     var fixme = document.querySelector('.con-con');
     var fixmeTop = 115
     var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    console.log(currentScroll,fixme.scrollTop)
+    console.log(currentScroll, fixme.scrollTop)
     if (currentScroll >= fixmeTop) {
         fixme.style.position = 'fixed';
         fixme.style.right = '0';
@@ -23,31 +23,36 @@ function scrollToElement(selector) {
 setTimeout(function () { scrollToElement("#home") }, 100)
 
 svgList = {
-    readmore: "data:image/svg+xml;base64,PHN2ZyBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGZpbGwtcnVsZT0iZXZlbm9kZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLW1pdGVybGltaXQ9IjIiIHZpZXdCb3g9IjAgMCAyNCAyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJtMTIuMDAyIDIxLjUzNGM1LjUxOCAwIDkuOTk4LTQuNDggOS45OTgtOS45OThzLTQuNDgtOS45OTctOS45OTgtOS45OTdjLTUuNTE3IDAtOS45OTcgNC40NzktOS45OTcgOS45OTdzNC40OCA5Ljk5OCA5Ljk5NyA5Ljk5OHptMC0xLjVjLTQuNjkgMC04LjQ5Ny0zLjgwOC04LjQ5Ny04LjQ5OHMzLjgwNy04LjQ5NyA4LjQ5Ny04LjQ5NyA4LjQ5OCAzLjgwNyA4LjQ5OCA4LjQ5Ny0zLjgwOCA4LjQ5OC04LjQ5OCA4LjQ5OHptMC02LjVjLS40MTQgMC0uNzUtLjMzNi0uNzUtLjc1di01LjVjMC0uNDE0LjMzNi0uNzUuNzUtLjc1cy43NS4zMzYuNzUuNzV2NS41YzAgLjQxNC0uMzM2Ljc1LS43NS43NXptLS4wMDIgM2MuNTUyIDAgMS0uNDQ4IDEtMXMtLjQ0OC0xLTEtMS0xIC40NDgtMSAxIC40NDggMSAxIDF6IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48L3N2Zz4=",
+    readmore: "data:image/svg+xml;utf8;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTYgMjU2Ij48cmVjdCB3aWR0aD0iMjU2IiBoZWlnaHQ9IjI1NiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik0yMDEuNTQxLDU0LjQ1OGExMDQsMTA0LDAsMSwwLDAsMTQ3LjA3ODEzQTEwNC4xMDg0NSwxMDQuMTA4NDUsMCwwLDAsMjAxLjU0MSw1NC40NThaTTE2My45OTQxNCwxNDcuOTk3MWE4LDgsMCwwLDEtMTYsMFYxMTkuMzA5NmwtNDIuMzQzNzUsNDIuMzQzNzVhNy45OTkxNSw3Ljk5OTE1LDAsMCwxLTExLjMxMjUtMTEuMzEyNWw0Mi4zNDM3NS00Mi4zNDM3NWgtMjguNjg3NWE4LDgsMCwwLDEsMC0xNmg0OGE4LjAyNzUzLDguMDI3NTMsMCwwLDEsOCw4WiIvPjwvc3ZnPg==",
+
 }
 
 var myWorks = [
     {
         title: "Placify App",
         content: "A mobile app designed to simplify college placements, providing personalized feeds, real-time job notifications, and updates to enhance the placement process for students.",
-        source: "https://www.linkedin.com/posts/idhanush_placements-backenddeveloper-techforgood-activity-7277985605483905027-lKnB?utm_source=share&utm_medium=member_desktop"
+        source: "https://www.linkedin.com/posts/idhanush_placements-backenddeveloper-techforgood-activity-7277985605483905027-lKnB?utm_source=share&utm_medium=member_desktop",
+        icon: "assets/placifylogo.svg"
     },
     {
         title: "IoT Robo V1",
         content: "An IoT-enabled animatronic greeting robot funded by IEDC SJC Devagiri, designed to interact with users through greetings and dynamic facial expressions.",
-        source: "https://github.com/iDhanush/IoT_Animatronics_Robot_V1.git"
+        source: "https://github.com/iDhanush/IoT_Animatronics_Robot_V1.git",
+        icon: ""
     },
     {
         title: "BGM4RealLife",
         content: "A fun project that adds cinematic background music to everyday moments and was featured in the top 10 \"Useless Projects\" for its creativity.",
-        source: "https://bgm4reallife.vercel.app/"
+        source: "https://bgm4reallife.vercel.app/",
+        icon: ""
     },
     {
         title: "LinkTools.in",
         content: "A web app for managing URLs with shortening, custom aliases, QR codes, analytics, and paywalled links. Perfect for personal and business use!",
-        source: "https://linktools.pages.dev/"
+        source: "https://linktools.pages.dev/",
+        icon: ""
     },
-    
+
 ]
 
 function createDiv(className) {
@@ -59,25 +64,27 @@ function createImg(src, className) {
     var img = document.createElement("img")
     img.src = src
     img.className = className
-    
+
     return img
 }
 var cards = document.querySelector(".cards")
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
-async function test () {
+async function test() {
     for (i = 0; i < myWorks.length; i++) {
         var card = createDiv("card")
         var contents = createDiv("contents")
         var title = createDiv("title")
         var readmore = createDiv("readmore")
         var content = createDiv("content")
+        // var project_icon = createImg(myWorks[i]["icon"], "icon")
 
         readmore.setAttribute("onclick", `window.open('${myWorks[i]['source']}')`)
         var readMoreImg = createImg(svgList["readmore"], "readmoreimg")
         readmore.appendChild(readMoreImg)
         title.innerHTML = myWorks[i]['title']
         title.appendChild(readmore)
+        
         content.innerHTML = myWorks[i]["content"]
         contents.appendChild(title)
         contents.appendChild(content)
